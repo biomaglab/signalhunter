a992092deaa198e753e608e9cfc49219378e2faf
vhosouza <victorhos@hotmail.com>
2016-09-26 09:02:43 -0300

    Reader multi Comma Separated Value files

--- reader/reader_multi.m
+++ reader/reader_multi.m
@@ -8,17 +8,31 @@ function output_reader = reader_multi
 
 % signal = load([pathname filename]);
 
-% works for CSV file
-[A, dlmout, hdrout] = importdata(f1); 
+[file_aux, path_aux, id] = uigetfile({'*.csv', 'Comma Separated Values'},...
+    'Import multiple files', 'MultiSelect', 'on');
 
-%get sampling frequency
-fs_str = A7.textdata{1};
-a1 = find(fs_str == '=');
-b1 = find (fs_str == '/');
-fs = str2double(fs_str(a1+2:b1-1));
-chans = str2double(fs_str(b1+1:end-1));
+n_files = size(file_aux, 2);
+file_aux = sort(file_aux);
+file_prop = cell(1, size(file_aux, 2));
 
-fs = A7.textdata{1};
+signal_par = struct;
+configuration_par = struct;
+data_aux = cell(1, size(file_aux,2));
+delimit_aux = cell(1, size(file_aux,2));
+hdr_aux = cell(1, size(file_aux,2));
+
+% read file data
+for i = 1:n_files
+    
+    [data_aux{1,i}, delimit_aux{1,i}, hdr_aux{1,i}] = importdata([path_aux file_aux{i}]); 
+    file_prop{1,i} = strsplit(file_aux{i}, {'_', '.'});
+    
+    % extract sampling frequency from comments
+    fs_str = data_aux{i}.textdata{1};
+    fs{i} = str2double(fs_str(find(fs_str == '=')+2:find((fs_str == '/'))-1));
+    chans{i} = str2double(fs_str(find((fs_str == '/'))+1:end));
+       
+end
 
 signal.Time = linspace(0,2*pi,100);
 % y = sin(signal.Time)';
