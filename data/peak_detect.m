edc2be35a636c56e641aa5f372c30bb4df16af35
lrakauskas <leorakauskas@gmail.com>
2017-02-02 20:50:38 -0200

    SH EMF Analysis adaptation

--- data/peak_detect.m
+++ data/peak_detect.m
@@ -20,6 +20,7 @@ else
     i = 1;
     
     for k = 1:(length(pulse_position)-1)
+        
         aux(i) = pulse_position(k+1) - pulse_position(k);
         
         % find pulse groups with distances higher than 4000 points
@@ -42,24 +43,27 @@ else
     
     clear aux j i 
     
+    figure
+%     plot(data,'.')
+%     hold on
     for j = 1:length(pulse(1,:))
         
         % calculates local maxima and process for high-frequency TMS noise
         % ignore
-        [~, aux] = max(pulse(:,j));
+        [~, aux] = max(abs(pulse(:,j)));
         
         % smooth signal around max value and find closest real value
-        smooth_amp = smooth(data((pulse_position(pulse_indice(aux))-4):...
-            (pulse_position(pulse_indice(aux))+4)));
-        abs_aux = abs(data((pulse_position(pulse_indice(aux))-4):...
-            (pulse_position(pulse_indice(aux))+4)) - max(smooth_amp));
+        smooth_amp = smooth(data((pulse_position(pulse_indice(aux,j))-4):...
+            (pulse_position(pulse_indice(aux,j))+4)));
+        abs_aux = abs(data((pulse_position(pulse_indice(aux,j))-4):...
+            (pulse_position(pulse_indice(aux,j))+4)) - max(smooth_amp));
         [~,min_aux] = min(abs_aux);
         
 %         smooth_max = dsearchn(data((pulse_position(pulse_indice(aux))-4)...
 %             :pulse_position((pulse_indice(aux)+4)))...
 %             ,max(smooth_amp));
-        smooth_max = (pulse_position(pulse_indice(aux))-4 + min_aux - 1);
-        pmax(j) = data(smooth_max);
+        smooth_max = (pulse_position(pulse_indice(aux,j))-4 + min_aux - 1);
+        pmax(j) = data(smooth_max); 
         peak(j) = smooth_max;
         
         % start and duration 
@@ -73,35 +77,46 @@ else
         
         cont_aux = 0;
         start_aux = 0;
-        
-         while (start_aux == 0)
-            for u = 1:length(amp_aux)
-           
-            if (amp_aux(u) >= mmax)
-                cont_aux = cont_aux + 1;
-                if cont_aux >= 5
-                    start_aux = u - 5;
+        u = 1;
+        while (start_aux == 0)
+            
+            if u > length(amp_aux)
+                cont_aux = 1000;
+            else
+                if (amp_aux(u) >= mmax)
+                    cont_aux = cont_aux + 1;
+                    if cont_aux >=5
+                        start_aux = u - 5;
+                    end
                 else
+                    cont_aux = 0;
                 end
             end
+            u = u + 1;
+        end
+        
+        cont_aux = 0;
+        end_aux = 0;
+        u = length(amp_aux);
+        
+        while (end_aux == 0)
+            u = u - 1;
+            if u < 1
+                end_aux = 1000;
+            else
             end
-         end
-         
-         cont_aux = 0;
-         end_aux = 0;
-         
-         while (end_aux == 0)
-            for u = length(amp_aux):1
-           
+            
             if (amp_aux(u) <= mmin)
                 cont_aux = cont_aux + 1;
                 if cont_aux >= 5
-                    end_aux = u - 5;
+                    end_aux = u + 5;
                 else
                 end
+            else
+                cont_aux = 0;
             end
-            end
-         end
+        end
+        
         
           
 %         
@@ -119,10 +134,20 @@ else
 %        diff_test = diff(data((pulse_position(aux)-4):(pulse_position(aux+4))));
 %        [~,max_diff] = max(diff_test);
 %        
-       start(j) = start_aux;
-       pulse_end(j) = end_aux;
-    
+       start(j) = (peak(j) - 300 + start_aux);
+       pulse_end(j) = (peak(j) - 300 + end_aux);
+       
+       
+       plot((peak(j)-300):(peak(j)+500),data(peak(j)-300):(peak(j)+500),'.')
+       hold on
+       plot(start,data(start),'.','color','r')
+       plot(pulse_end,data(pulse_end),'.','color','r')
+       
+       
+      waitforbuttonpress
+      hold off
     end
+    
     
 end
 end
