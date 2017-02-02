3572fe3414634f336ac89c3bfe2cfc2044e76427
lrakauskas <leorakauskas@gmail.com>
2017-02-02 16:28:18 -0200

    EMF Analysis adaptation

--- data/peak_detect.m
+++ data/peak_detect.m
@@ -49,44 +49,78 @@ else
         [~, aux] = max(pulse(:,j));
         
         % smooth signal around max value and find closest real value
-        smooth_amp = smooth(data((pulse_position(aux)-4):(pulse_position(aux+4))));
-        smooth_max = dsearchn(data((pulse_position(aux)-4):(pulse_position(aux+4)))...
-            ,max(smooth_amp));
+        smooth_amp = smooth(data((pulse_position(pulse_indice(aux))-4):...
+            (pulse_position(pulse_indice(aux))+4)));
+        abs_aux = abs(data((pulse_position(pulse_indice(aux))-4):...
+            (pulse_position(pulse_indice(aux))+4)) - max(smooth_amp));
+        [~,min_aux] = min(abs_aux);
         
-        pmax(j) = data(smooth_max,j);
-        peak(j) = (smooth_max);
+%         smooth_max = dsearchn(data((pulse_position(pulse_indice(aux))-4)...
+%             :pulse_position((pulse_indice(aux)+4)))...
+%             ,max(smooth_amp));
+        smooth_max = (pulse_position(pulse_indice(aux))-4 + min_aux - 1);
+        pmax(j) = data(smooth_max);
+        peak(j) = smooth_max;
         
         % start and duration 
+        
         
         amp_aux = data((peak(j)-300):(peak(j)+500));
         mean_bef = mean(data((peak(j)-300):(peak(j)-5)));
         sd_bef = std(data((peak(j)-300):(peak(j)-5)));
-        mmax = mean_bef + 3.5*sd_bef;
-        mmin = mean_bef - 3.5*sd_bef;
+        mmax = mean_bef + 2*sd_bef;
+        mmin = mean_bef - 2*sd_bef;
         
-        for u = 1:length(amp_aux)
-            if (amp_aux(u) <= mmax && amp_aux(u) >= mmin)
-                amp_aux2(u) = mean_bef;
-            else
-                amp_aux2(u) = amp_aux(u);
+        cont_aux = 0;
+        start_aux = 0;
+        
+         while (start_aux == 0)
+            for u = 1:length(amp_aux)
+           
+            if (amp_aux(u) >= mmax)
+                cont_aux = cont_aux + 1;
+                if cont_aux >= 5
+                    start_aux = u - 5;
+                else
+                end
             end
-        end
+            end
+         end
+         
+         cont_aux = 0;
+         end_aux = 0;
+         
+         while (end_aux == 0)
+            for u = length(amp_aux):1
+           
+            if (amp_aux(u) <= mmin)
+                cont_aux = cont_aux + 1;
+                if cont_aux >= 5
+                    end_aux = u - 5;
+                else
+                end
+            end
+            end
+         end
         
-        amp_aux3 = diff(amp_aux2);
-        
-        [~, max_pos] = max(amp_aux3);
-        [~, min_pos] = min(amp_aux3);
-        
-        max_pos = max_pos - 300 - 1;
-        min_pos = min_pos - 300 + 1;
-        
-       peak(j) = max_pos;
-       pulse_end(j) = min_pos+1;
-       
-       diff_test = diff(data((pulse_position(aux)-4):(pulse_position(aux+4))));
-       [~,max_diff] = max(diff_test);
-       
-       start(j) = ceil(max_diff - 1);
+          
+%         
+%         amp_aux3 = diff(amp_aux2);
+%         
+%         [~, max_pos] = max(amp_aux3);
+%         [~, min_pos] = min(amp_aux3);
+%         
+%         max_pos = max_pos - 300 - 1;
+%         min_pos = min_pos - 300 + 1;
+%         
+%        peak(j) = max_pos;
+%        pulse_end(j) = min_pos+1;
+%        
+%        diff_test = diff(data((pulse_position(aux)-4):(pulse_position(aux+4))));
+%        [~,max_diff] = max(diff_test);
+%        
+       start(j) = start_aux;
+       pulse_end(j) = end_aux;
     
     end
     
